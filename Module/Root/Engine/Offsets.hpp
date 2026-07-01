d68c7d785ea397eb757042705edcd92d2c230ca3
maki-lamg <glassmanocean4@gmail.com>
2026-07-01 12:24:05 -0400

    updated version

--- Module/Root/Engine/Offsets.hpp
+++ Module/Root/Engine/Offsets.hpp
@@ -11,31 +11,31 @@ inline const uintptr_t HYPERION = (uintptr_t)GetModuleHandle("RobloxPlayerBeta.d
 namespace Offsets {
 
     const uintptr_t Bitmap = HYP_REBASE(0x1C1280);
-    const uintptr_t Cast_To_Varient = REBASE(0x1CCF0A0);
-    const uintptr_t FireMouseClick = REBASE(0x2611D70);
-    const uintptr_t FireMouseHoverEnter = REBASE(0x2611F80);
-    const uintptr_t FireMouseHoverLeave = REBASE(0x2612170);
-    const uintptr_t FireProximityPrompt = REBASE(0x2653780);
-    const uintptr_t FireRightMouseClick = REBASE(0x26123E0);
-    const uintptr_t FireTouchInterest = REBASE(0x2A50730);
-    const uintptr_t GetIdentityStruct = REBASE(0x47166A0);
+    const uintptr_t Cast_To_Varient = REBASE(0x1CE53A0);
+    const uintptr_t FireMouseClick = REBASE(0x262F070);
+    const uintptr_t FireMouseHoverEnter = REBASE(0x262F280);
+    const uintptr_t FireMouseHoverLeave = REBASE(0x262F470);
+    const uintptr_t FireProximityPrompt = REBASE(0x266F870);
+    const uintptr_t FireRightMouseClick = REBASE(0x262F6E0);
+    const uintptr_t FireTouchInterest = REBASE(0x2A806C0);
+    const uintptr_t GetIdentityStruct = REBASE(0x47604C0);
     const uintptr_t GetLuaState = REBASE(0x1CEDE20);
-    const uintptr_t GetModuleFromVMStateMap = REBASE(0x1D1CF00);
-    const uintptr_t GetProperty = REBASE(0xC88A20);
-    const uintptr_t IdentityPtr = REBASE(0x816A140);
-    const uintptr_t Impersonator = REBASE(0x1CCE250);
-    const uintptr_t KTable = REBASE(0x817A980);
-    const uintptr_t LuaC_step = REBASE(0x46BAC60);
-    const uintptr_t LuaD_throw = REBASE(0x46B6310);
-    const uintptr_t LuaH_DummyNode = REBASE(0x6933598);
-    const uintptr_t LuaH_getn = REBASE(0x46D8560);
-    const uintptr_t LuaO_NilObject = REBASE(0x69336F0);
-    const uintptr_t LuaVM_Load = REBASE(0x1D06510);
-    const uintptr_t Luau_Execute = REBASE(0x46C8A40);
-    const uintptr_t opcodetablelookup = REBASE(0x61C76E0);
-    const uintptr_t Print = REBASE(0x1E6B8E0);
-    const uintptr_t PushInstance1 = REBASE(0x1CE0CC0);
-    const uintptr_t TaskSchedulerTargetFps = REBASE(0x8179080);
+    const uintptr_t GetModuleFromVMStateMap = REBASE(0x1D33260);
+    const uintptr_t GetProperty = REBASE(0xC8A490);
+    const uintptr_t IdentityPtr = REBASE(0x81BD8E0);
+    const uintptr_t Impersonator = REBASE(0x1CE53A0);
+    const uintptr_t KTable = REBASE(0x81CE200);
+    const uintptr_t LuaC_step = REBASE(0x4704C60);
+    const uintptr_t LuaD_throw = REBASE(0x47003E0);
+    const uintptr_t LuaH_DummyNode = REBASE(0x6983298);
+    const uintptr_t LuaH_getn = REBASE(0x4722360);
+    const uintptr_t LuaO_NilObject = REBASE(0x69833F0);
+    const uintptr_t LuaVM_Load = REBASE(0x1D1C840);
+    const uintptr_t Luau_Execute = REBASE(0x47128F0);
+    const uintptr_t opcodetablelookup = REBASE(0x62176B0);
+    const uintptr_t Print = REBASE(0x4681920);
+    const uintptr_t PushInstance1 = REBASE(0x1CF6FB0);
+    const uintptr_t TaskSchedulerTargetFps = REBASE(0x81CC870);
 
     namespace External {
         namespace Bytecode {
@@ -52,7 +52,7 @@ namespace Offsets {
         }
         namespace TaskScheduler {
             constexpr uintptr_t FakeDMtoDM = 0x1C0;
-            const uintptr_t FakeDMPointer = REBASE(0x7FF0818);
+            const uintptr_t FakeDMPointer = REBASE(0x81CC868);
             const uintptr_t ScriptContext = 0x3F0;
             constexpr uintptr_t PlaceId = 0x190;
             constexpr uintptr_t JobEnd = 0x1D8;
--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 cobalt executor
-## version 1a951716f19e4638
+## version-5cf2272675e145f5
 you will need to compile it yourself, lowk jus ask ai to do it or watch a youtube tutorial
 
 
